129f0d57a059ae98abb76aedc3075ab58c1e8746
Sibin Xavier <sibinx7@gmail.com>
2017-08-01 22:17:15 +0530

    Add particle js

--- app/components/home/home-slider.js
+++ app/components/home/home-slider.js
@@ -1,4 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  init(){
+    this._super(...arguments)
+    if(typeof particlesJS !="undefined"){
+      particlesJS.load('particles-js', 'other/particles.json', function() {
+        console.log('callback - particles.js config loaded');
+      });
+    }
+  }
 });
